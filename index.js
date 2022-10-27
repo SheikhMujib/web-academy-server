4a39c15f7a7dc47b89b2696d39a592c98ad03336
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-27 22:46:45 +0600

    Vercel deploy complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./data/courses.json");
+const details = require("./data/details.json")
 
 app.get("/", (req, res) => {
   res.send("Web Academy server is runing.");
@@ -14,6 +15,12 @@ app.get("/", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+
+app.get("/details/:id", (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = details.find(d => d.id === id);
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
   console.log("Web Academy server is runing on port", port);
