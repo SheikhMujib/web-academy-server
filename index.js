e4cb2d401da87a13b3526b93a3762c6cd0f8dac3
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-27 19:00:20 +0600

    Courses data api created

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
-    res.send('Web Academy server is runing.')
-})
+app.use(cors());
 
-app.listen(port, ()=>{
-    console.log('Web Academy server is runing on port', port)
-})
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("Web Academy server is runing.");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => {
+  console.log("Web Academy server is runing on port", port);
+});
